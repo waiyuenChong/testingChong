c7dbd5bfb49c28a7a639e6c0b05a908b7729eb3c
7techtf <teinfai.cheng@vigrab.com>
2022-04-04 17:16:07 +0800

    123

--- ci4_new/app/Views/errorpage.php
+++
@@ -1,5 +0,0 @@
-<section class="error_container">
-    <div class="error_content">
-        <div class="error_text"><?=lang("page_not_found") ?></div>
-    </div>
-</section>
--- ci4_new/public/assets/js/action.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('.lang_switcher').on('change', function() {
-		var lang = $(this).val();
-		window.location = ajaxBaseUrl + "/" + lang + router;
-	});
-});
