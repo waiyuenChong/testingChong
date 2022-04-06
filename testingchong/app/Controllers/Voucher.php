17f8a9f7f18d9967487cf24e8c074821ce2f24ce
RubyLing26 <92001225+RubyLing26@users.noreply.github.com>
2022-04-06 16:01:17 +0800

    done update

--- testingchong/app/Controllers/Voucher.php
+++ testingchong/app/Controllers/Voucher.php
@@ -37,29 +37,28 @@ class Voucher extends MY_Controller
 
         $data               = array(); // define as an arary and store data that get from API
         if ($_POST) {
-            // preview($_POST);die;     
-            $create_voucher = array(
-                'table' => 'Voucher',
-                'data'  => array(
-                    'id'    => $_POST['voucher_id'],
-                    'user_id'   => $_POST['user_id'],
-                    'user_name' => $_POST['user_name'],
-                    'voucher_type' => $_POST['voucher_type'],
-                    'voucher_quantity' => $_POST['voucher_quantity'],
-                    'leave_type_id' => $_POST['leave_type_id'],
-                ),
-            );
-             $response = $this->model->DataInsert($create_voucher);
+            // preview($_POST);die;    
+            if ($_POST['type' == 1]) {
+                $create_voucher = array(
+                    'table' => 'Voucher',
+                    'data'  => array(
+                        'id'    => $_POST['voucher_id'],
+                        'user_id'   => $_POST['user_id'],
+                        'user_name' => $_POST['user_name'],
+                        'voucher_type' => $_POST['voucher_type'],
+                        'voucher_quantity' => $_POST['voucher_quantity'],
+                        'leave_type_id' => $_POST['leave_type_id'],
+                    ),
+                );
+            };
 
 
-            // if ($_POST) {
-            //     preview($_POST);
-            //     die;
-            // } else {    
-            //     preview('fail to insert');
-            //     die;
-            // }
-        }
+
+
+            $response = $this->model->DataInsert($create_voucher);
+        };
+
+
 
 
 
@@ -70,10 +69,8 @@ class Voucher extends MY_Controller
             'table'     => 'Voucher',
             'select'    => 'id, user_id, user_name, voucher_type, voucher_quantity, leave_type_id',
         );
-        $response = $this->model->getWhere($table_voucher);
 
-        $data['response'] = isset($response) ? $response : [];
-        
+
 
 
         $table_leave = array(
@@ -82,17 +79,43 @@ class Voucher extends MY_Controller
             'condition'    => array('status' => 1),
             // 'condition' => 'status = 1',
         );
+
+
+        $response = $this->model->getWhere($table_voucher);
         $response1 = $this->model->getWhere($table_leave);
 
+        $data['response'] = isset($response) ? $response : [];
+        $data['response1'] = isset($response1) ? $response1 : [];
 
 
-        $data['response1'] = isset($response1) ? $response1 : []; 
 
-
-        
         // preview($data);die;  
         echo view('templates/header', $header);
         echo view('voucher/list', $data);
         echo view('templates/footer', $footer);
+    }
+
+    public function update($voucher_id = "")
+    {
+        if ($_POST) {
+            // preview($_POST);die;
+            if ($_POST['update'] == 0) {
+                $update_verify = $this->model->DataUpdate([
+                    'table'     => 'Voucher',
+                    'data'      => [
+                        'user_name' => $_POST['user_name_update'],
+                        'voucher_type' => $_POST['voucher_type_update'],
+                        'voucher_quantity' => $_POST['voucher_quantity_update'],
+                        'leave_type_id' => $_POST['leave_type_id_update'],
+                    ],
+                    'condition' => [
+                        'id' => $voucher_id,
+                    ],
+
+                ]);
+
+                return redirect()->to(site_url('/voucher/ruby'));
+            }
+        }
     }
 }
--- testingchong/app/Views/voucher/list.php
+++ testingchong/app/Views/voucher/list.php
@@ -63,7 +63,7 @@ echo form_open_multipart('voucher/ruby', $attributes);
                         </div>
                         <div class="row mt-5 justify-content-end">
                             <div class="col-3 text-end pe-0">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary" value="1" name="type">Submit</button>
                                 <div class="row mt-5 justify-content-end">
                                     <div class="col-3 text-end pe-0">
                                     </div>
@@ -76,6 +76,7 @@ echo form_open_multipart('voucher/ruby', $attributes);
             </div>
         </div>
     </div>
+    </form>
 
     <br>
     <br>
@@ -106,9 +107,69 @@ echo form_open_multipart('voucher/ruby', $attributes);
                                     <td><?= $value->voucher_type ?></td>
                                     <td><?= $value->voucher_quantity ?></td>
                                     <td><?= $value->leave_type_id ?></td>
-                                    <td> <button class="btn btn-update" type="submit" id="update" name="update" value="1" onclick="loader()"><?= "Update" ?></button></td>
+                                    <td> <button class="btn btn-update" type="button" data-bs-toggle="modal" data-bs-target="#update_modal_<?= $value->id ?>">Update</button></td>
                                 </tr>
                 </div>
+                <?php
+                                $attributes = array('class' => 'add_agent', 'id' => 'add_agent_form', "onsubmit" => "loader()", 'autocomplete' => "off");
+                                echo form_open_multipart('voucher/update/' . $value->id, $attributes);
+                ?>
+                <div class="modal" tabindex="-1" role="dialog" id="update_modal_<?= $value->id ?>">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title"><?= $value->id ?></h5>
+                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <!-- <input type="text" name="id" value="<?= $value->id ?>" hidden> -->
+                            <div class="modal-body">
+                                <p><?= "User ID = $value->user_id " ?></p>
+                                <div class="row mb-3">
+                                    <div class="col-12">
+                                        <label for="">User Name</label>
+                                        <input class="form-control" type="text" id="user_name_update" name="user_name_update">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <label for="">Voucher Type</label>
+                                    <select id="voucher_type_update" name="voucher_type_update">
+                                        <option value='Cash Voucher'>Cash Voucher</option>
+                                        <option value='Rebate Voucher'>Rebate Voucher</option>
+                                        <option value='Discount Voucher'>Disount Voucher</option>
+                                    </select>
+                                </div>
+                                <div class="col-12">
+                                    <label for="">Voucher Quantity (max 3)</label>
+                                    <select id="voucher_quantity_update" name="voucher_quantity_update">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                                <div class="row mb-5">
+                                    <div class="col-12">
+                                        <label for="">Leave Type</label>
+                                        <select name="leave_type_id_update" id="leave_type_id_update">
+                                            <?php foreach ($response1 as $key => $value) { ?>
+                                                <option value="<?= $value->id ?>"> <?= $value->name ?></option>
+                                            <?php }
+                                            ?>
+                                        </select>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary" value="0" name="update">Save changes</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </form>
 
             <?php } ?>
             </tbody>
@@ -117,4 +178,7 @@ echo form_open_multipart('voucher/ruby', $attributes);
             </div>
         </div>
     </div>
+
+
+
 </div>
